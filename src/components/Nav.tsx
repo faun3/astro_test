ce43acc90e6964bb9de9104df3d9451182aded7d
faun3 <antonioculcea@gmail.com>
2023-08-22 10:38:01 +0300

    write navbar component and import it

---
+++ src/components/Nav.tsx
@@ -0,0 +1,15 @@
+import logoIcon from "/favicon.svg";
+
+const Nav = () => {
+  return (
+    <nav className="py-2 px-4 flex flex-row items-center gap-4">
+      <div className="w-10 h-10">
+        <img src={logoIcon} alt="" />
+      </div>
+      <span className="text-white tracking-wider font-bold text-xl">
+        AmazingNav
+      </span>
+    </nav>
+  );
+};
+export default Nav;
